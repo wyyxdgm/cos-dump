9445f12cd514dd5949234db170f98ff0f3fa9954
Damo <daiguimao@sightp.com>
2024-11-19 07:00:29 +0800

    fixes

--- bin/cos-dump-cli.js
+++ bin/cos-dump-cli.js
@@ -3,18 +3,6 @@ const { listAndDownloadFiles } = require("../lib/cos-dump");
 
 // 命令行参数解析
 const argv = yargs
-  .option("dir", {
-    alias: "d",
-    description: "下载目录",
-    type: "string",
-    default: "downloads",
-  })
-  .option("ckpt", {
-    alias: "c",
-    description: "检查点文件位置",
-    type: "string",
-    default: "checkpoint.json",
-  })
   .option("secret-id", {
     alias: "s",
     description: "SecretId",
@@ -39,39 +27,37 @@ const argv = yargs
     type: "string",
     demandOption: false,
   })
+  .option("dir", {
+    alias: "d",
+    description: "下载目录",
+    type: "string",
+    default: "downloads",
+  })
+  .option("ckpt", {
+    alias: "c",
+    description: "检查点文件位置",
+    type: "string",
+    default: "checkpoint.json",
+  })
+  .option("fail-path", {
+    alias: "f",
+    description: "错误信息存储路径",
+    type: "string",
+    demandOption: false,
+  })
   .help()
   .alias("help", "h").argv;
-if (argv.SecretId) {
-  envConfig.SecretId = argv.SecretId;
-}
-const envConfig = {
-  SecretId: process.env.COS_SECRETID,
-  SecretKey: process.env.COS_SECRETKEY,
-  Bucket: process.env.COS_BUCKET,
-  Region: process.env.COS_REGION,
-  dir: process.env.COS_DIR,
-  ckpt: process.env.COS_CKPT,
-  failPath: process.env.COS_FAILPATH,
-};
 
-if (argv.SecretKey) {
-  envConfig.SecretKey = argv.SecretKey;
-}
-if (argv.Bucket) {
-  envConfig.Bucket = argv.Bucket;
-}
-if (argv.Region) {
-  envConfig.Region = argv.Region;
-}
-if (argv.dir) {
-  envConfig.dir = argv.dir;
-}
-if (argv.ckpt) {
-  envConfig.ckpt = argv.ckpt;
-}
-if (argv.failPath) {
-  envConfig.failPath = argv.failPath;
-}
+// 初始化配置对象
+const envConfig = {
+  SecretId: argv["secret-id"] || process.env.COS_SECRETID,
+  SecretKey: argv["secret-key"] || process.env.COS_SECRETKEY,
+  Bucket: argv.bucket || process.env.COS_BUCKET,
+  Region: argv.region || process.env.COS_REGION,
+  dir: argv.dir || process.env.COS_DIR,
+  ckpt: argv.ckpt || process.env.COS_CKPT,
+  failPath: argv["fail-path"] || process.env.COS_FAILPATH,
+};
 
 // 调用封装后的函数
 listAndDownloadFiles(envConfig)
